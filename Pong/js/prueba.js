bc239eaa349b1767501d24f4c237dcf153de0e10
pwnGIGO <Rodrigo>
2021-07-18 04:28:08 -0500

    movimiento de teclas

--- Pong/js/prueba.js
+++ Pong/js/prueba.js
@@ -1,6 +1,5 @@
 PIXI.utils.sayHello(); // the code from Step 3, you can leave it
 // create a canvas for all of your game elements
-
 const renderer = PIXI.autoDetectRenderer(720, 512, {
   transparent: true,
   resolution: 1
@@ -13,9 +12,10 @@ const stage = new PIXI.Container();
 // lets continue our code
 // load the spritesheet, first arg could name anything you want
 // second arg will be the path of the png file
+
 PIXI.loader
-  .add("spritesheet", "Esqueletos.png")
-  .load(setup);
+.add("spritesheet", "assets/img/Nube.png")
+.load(setup);
 
 var sprite; // given a global variable, we will be using it 
 function setup() {
@@ -35,8 +35,8 @@ function setup() {
   // highly recommend to use scale to change frame size
   sprite.scale.set(2, 2); 
   // these two lines are for eventlistener later
-  sprite.vx = 10;
-  sprite.vy = 10;
+  sprite.vx = 5;
+  sprite.vy = 5;
   stage.addChild(sprite); // add sprite to stage area
   animationLoop(); // from below helper function
 }
@@ -47,16 +47,64 @@ function animationLoop() {
   // a function from Pixi
   requestAnimationFrame(animationLoop);
   renderer.render(stage);
+  mueveJugador()
 };
 
-window.addEventListener('keydown', event => {
-  if (event.key === 'ArrowRight' && sprite.x < 650) {
-    sprite.x += sprite.vx
-  } else if (event.key === 'ArrowLeft' && sprite.x > 0) {
+
+// Banderas de teclas
+let left, right, up, down;
+this.down = false;
+this.left = false;
+this.up = false;
+this.right = false;
+
+function mueveJugador() {
+  if (left && up) {
     sprite.x -= sprite.vx
-  } else if (event.key === 'ArrowDown' && sprite.y < 450) {
-    sprite.y += sprite.vy
-  } else if (event.key === 'ArrowUp' && sprite.y > 0) {
     sprite.y -= sprite.vy
+  } else if (left && down) {
+    sprite.x -= sprite.vx
+    sprite.y += sprite.vy
+  } else if (right && up) {
+    sprite.x += sprite.vx
+    sprite.y -= sprite.vy
+  } else if (right && down) {
+    sprite.x += sprite.vx
+    sprite.y += sprite.vy
+  } else if (left) {
+    sprite.x -= sprite.vx
+  } else if (up) {
+    sprite.y -= sprite.vy
+  } else if (right) {
+    sprite.x += sprite.vx
+  } else if (down) {
+    sprite.y += sprite.vx
+  }
+}
+
+window.addEventListener('keydown', event => {
+  //console.log(event.key)
+  if (event.key === 'ArrowRight') {
+    right = true;
+  } else if (event.key === 'ArrowLeft') {
+    left = true;
+  } else if (event.key === 'ArrowDown') {
+    down = true;
+  } else if (event.key === 'ArrowUp') {
+    up = true;
+  }
+})
+
+
+window.addEventListener('keyup', event => {
+  //console.log(event.key)
+  if (event.key === 'ArrowRight') {
+    right = false;
+  } else if (event.key === 'ArrowLeft') {
+    left = false;
+  } else if (event.key === 'ArrowDown') {
+    down = false;
+  } else if (event.key === 'ArrowUp') {
+    up = false;
   }
 })
